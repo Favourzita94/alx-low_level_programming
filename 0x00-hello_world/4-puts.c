8c90c11f2c5b1a204a1b6c7e5162ddb08efd020c
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-02 09:46:33 -0700

    Commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 2 
 3 /**
 4 * main - Entry point
-5 *
-6  * Return: Always 0 (Success)  
+5 * Description: prints "\"Programming is like building a multilingual puzzle"
+6  * Return: 0
 7 */
-8  int main(void)
-9 {
-10            puts("\"Programming is like building a multilingual puzzle");
-11            return (0);
-12 }
+8 
+9 int main(void)
+10 {
+11               puts("\"Programming is like building a multilingual puzzle");
+12               return (0);
+13 }
