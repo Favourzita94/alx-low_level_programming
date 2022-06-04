9ca4a4d2aa30b1f566f3a50e58a04cc6fe580975
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-04 15:20:11 -0700

    Commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,14 +18,18 @@ int main(void)
 			if (d != c && d < c)
 			{
 				putchar('0' + d);
-				putchar(' ');
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
 			}
 		}
 		c++;
 	}
 	d++;
 }
-putchar('\n');
 putchar('\n');
 }
 
