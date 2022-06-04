eb35dd1b6753db3912e46916c33a019cc9c1f72d
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-04 16:00:04 -0700

    Commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,18 @@
  */
 int main(void)
 {
-int num;
-for (num = 0; num  <= 9; num++)
-{
-	putchar((num % 10) + '0');
-	if (num == 9)
-		continue;
-	putchar(',');
-	putchar(',');	
-}
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
 	putchar('\n');
 	return (0);
 }
