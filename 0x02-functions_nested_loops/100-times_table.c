d739fa96a1a04450f54606ebf038188561a0dd1a
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-08 09:44:05 -0700

    Commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,6 +22,7 @@ void print_times_table(int n)
 				if (column == 0)
 					_putchar('0');
 				else if (product < 10)
+
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -29,7 +30,9 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product % 10 + '0');
 				}
+
 				else if (product >= 10 && product < 100)
+
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -43,11 +46,12 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product / 100 + '0');
 					_putchar((product / 10) % 10 + '0');
-					_putchar((product / % 10 + '0');
+					_putchar(product % 10 + '0');
+
 				}
+		
 			}
 			_putchar('\n');
 		}
 	}
 }
-
