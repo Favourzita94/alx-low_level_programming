01fe56a35276e84bc2f639be990a4e4b00c2513b
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-08 08:28:05 -0700

    Commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,9 +7,10 @@
 void times_table(void)
 {
 	int i, j, n;
-	for (i = 0; i <= 9; i++);
+	
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++);
+		for (j = 0; j <= 9; j++)
 		{
 			n = i * j;
 			if ((n / 10) == 0)
