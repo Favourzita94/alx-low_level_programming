83d193d7f90ee18da9ed86d320028491d42430b4
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-08 06:38:43 -0700

    Commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,25 @@
 #include "main.h"
 /**
- *
+*
+* main - entry bloc* @void: no argument
+* Return: 0
+**/
+int main(void)
+
+{
+	char ermias[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+
+	{
+
+		_putchar(ermias[c]);
+
+									
+	}
+
+	_putchar('\n');
+	return (0);
+}
