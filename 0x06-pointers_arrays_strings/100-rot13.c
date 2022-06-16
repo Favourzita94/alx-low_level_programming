85a9b23e4d40ce73a497b85978e5bb14d01bf409
Favourzita94 <amarachinkemdilim@yahoo.com>
2022-06-16 05:07:46 -0700

    Commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,22 +12,22 @@ char *rot13(char *s)
 		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
 		'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
 		'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-		'Z'};
+		 'Z'};
 	char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-		'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',		'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'			'M'};
-
+	       'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	       'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+	       'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+		'M'};
 	for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
 	{
 		for (rotation = 0; rotation < 53; rotation++)
 		{
 			if (r1[rotation] == s[stringCount])
 			{
-				s[stringCount] = r2[rotation]
+				s[stringCount] = r2[rotation];
 				break;
 			}
 		}
 	}
 	return (s);
 }
-
